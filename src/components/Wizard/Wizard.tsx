0129c1b3927d60bbdf491bd045c0f37e814c60e1
Richard Fernandez <richard@noumena.global>
2022-02-15 11:20:05 +0800

    Wizard lint appeasing

--- src/components/Wizard/Wizard.tsx
+++ src/components/Wizard/Wizard.tsx
@@ -1,14 +1,14 @@
-export const Wizard = () => {
-  return (
-    <>
-      <div>
-        <div>Page 1</div>
-        <div>Page 2</div>
-        <div>Page 3</div>
-      </div>
-      <div>
-        <button>Next Page</button>
-      </div>
-    </>
-  );
-};
+export const Wizard = () => (
+  <div>
+    <div>
+      <div>Page 1</div>
+      <div>Page 2</div>
+      <div>Page 3</div>
+    </div>
+    <div>
+      <button type="button">Next Page</button>
+    </div>
+  </div>
+);
+
+export default Wizard;
